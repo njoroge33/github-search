48791581025de7f590a5dcce98cce5e37107f9f0
njoroge33 <johngichuhi769@gmail.com>
2019-11-02 23:18:53 +0300

    add content to profile service

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,23 +1,23 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient, HttpHeaders } from '@angular/common/http'
-// import { environment } from 'src/environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class ProfileService {
-//   private username: string;
-//   private clientid = "";
-//   private clientsecret = "";
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+  private username: string;
+  private clientid = "";
+  private clientsecret = "";
 
-//   constructor(private http: HttpClient) { 
-//     console.log('service is now ready');
-//     this.username = 'njoroge33'
-//   }
+  constructor(private http: HttpClient) { 
+    console.log('service is now ready');
+    this.username = 'njoroge33'
+  }
 
-//   getProfile() {
-//     return this.http.get('https://api.github.com/users/' + this.username, ({
-//       headers: new HttpHeaders({Authorization: `token ${environment.apikey}`})
-//     }))
-//   }
-// }
+  getProfile() {
+    return this.http.get('https://api.github.com/users/' + this.username, ({
+      headers: new HttpHeaders({Authorization: `token ${environment.apikey}`})
+    }))
+  }
+}
