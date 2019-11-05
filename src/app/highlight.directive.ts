ac875ee5cf9cbce58b19a97fff85fd2bf6fd0f1c
njoroge33 <johngichuhi769@gmail.com>
2019-11-05 13:26:45 +0300

    correct  the target of the highlighting directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -7,7 +7,7 @@ export class HighlightDirective {
   constructor(private el: ElementRef) { }
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.highlight('yellow');
+    this.highlight('gray');
   }
 
   @HostListener('mouseleave') onMouseLeave() {
